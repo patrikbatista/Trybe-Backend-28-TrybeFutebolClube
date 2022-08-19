4063c6adc3d3f23326d0e4299de92a18a3631438
patrik <pcr_batista@hotmail.com>
2022-08-19 16:13:19 -0300

    feat: implementa migrations de match

---
+++ app/backend/src/database/migrations/20220818220956-create-matches.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      }
+
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  },
+};
