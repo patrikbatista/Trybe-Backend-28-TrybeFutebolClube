430911b8dae92039ceccce11c3c9ce037a9ab2da
patrik <pcr_batista@hotmail.com>
2022-08-24 16:17:43 -0300

    feat: inclui a rota de validate token

--- app/backend/src/routes/userRoutes.ts
+++ app/backend/src/routes/userRoutes.ts
@@ -1,4 +1,5 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import LoginBodyValidate from '../middlewares/loginBodyValidate';
 import LoginService from '../services/loginServices';
 import LoginController from '../controllers/loginController';
 import User from '../database/models/user';
@@ -12,12 +13,17 @@ export default class UserRouter {
   constructor(endpoint: string) {
     this.router = Router();
     this.endpoint = endpoint;
+
     this.service = new LoginService(User);
     this.controller = new LoginController(this.service);
   }
 
   public addRoute() {
-    this.router.post(`/${this.endpoint}`, this.controller.create);
-    this.router.get(`/${this.endpoint}/validate`, (_req: Request, res: Response) => res.send('ok'));
+    this.router.post(`/${this.endpoint}`, LoginBodyValidate.validateLogin, this.controller.create);
+    this.router.get(
+      `/${this.endpoint}/validate`,
+      LoginBodyValidate.tokenValidate,
+      LoginController.validatedToken,
+    );
   }
 }
