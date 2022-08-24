3651ed14fc135633e79132bbeaa56fdfba17d1ce
patrik <pcr_batista@hotmail.com>
2022-08-24 16:20:27 -0300

    feat: implementa validacao do login

---
+++ app/backend/src/middlewares/loginBodyValidate.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import validator from 'validator';
+import verifyToken from '../utils/verifyToken';
+
+export default class LoginBodyValidate {
+  public static validateLogin(req: Request, res: Response, next:NextFunction) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: 'Field email invalid' });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Field password invalid' });
+    }
+    return next();
+  }
+
+  public static tokenValidate(req: Request, res: Response, next:NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+    const user = verifyToken(authorization);
+    if (!user) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+
+    req.body.user = user;
+    next();
+  }
+}
