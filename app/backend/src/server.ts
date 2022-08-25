5a26a0b23e005157aefcfeda2940dd18f2fef829
patrik <pcr_batista@hotmail.com>
2022-08-25 11:18:50 -0300

    feat: instancia team route no server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,14 +1,20 @@
 import { App } from './app';
 import 'dotenv/config';
 import UserRouter from './routes/userRoutes';
+import TeamRoutes from './routes/teamRoutes';
 
 const PORT = process.env.APP_PORT || 3001;
 
 // new App().start(PORT);
 
 const app = new App();
+
 const userRouter = new UserRouter('login');
 userRouter.addRoute();
 app.addRouter(userRouter.router);
+
+const teamRoutes = new TeamRoutes('teams');
+teamRoutes.addRoute();
+app.addRouter(teamRoutes.router);
 
 app.start(PORT);
