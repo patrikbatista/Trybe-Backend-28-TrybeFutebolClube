3e50dc7cd19021ff4c7c018abebe2cdc46ae8e8d
patrik <pcr_batista@hotmail.com>
2022-08-25 11:17:21 -0300

    feat: implementa controller de team

---
+++ app/backend/src/controllers/teamsControllers.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import TeamService from '../services/teamService';
+
+export default class TeamController {
+  private teamService: TeamService;
+
+  constructor(teamService: TeamService) {
+    this.teamService = teamService;
+    this.getAll = this.getAll.bind(this);
+    this.getById = this.getById.bind(this);
+  }
+
+  public async getAll(req: Request, res: Response, _next:NextFunction) {
+    const teams = await this.teamService.getAll();
+    res.status(200).json(teams);
+  }
+
+  public async getById(req: Request, res: Response, _next:NextFunction) {
+    const { id } = req.params;
+    if (id !== undefined) {
+      const team = await this.teamService.getById(Number(id));
+      if (team !== null) {
+        return res.status(200).json(team);
+      }
+      return res.status(404).json({ message: 'Team not found' });
+    }
+    return res.status(401).json({ message: 'id is required' });
+  }
+}
