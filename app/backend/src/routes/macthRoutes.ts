f9eceb9273b8510f1669e97d38571bee34195c59
patrik <pcr_batista@hotmail.com>
2022-08-26 22:26:04 -0300

    feat: implemeta as rotas de match

--- app/backend/src/routes/macthRoutes.ts
+++ app/backend/src/routes/macthRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import MatchBodyValidate from '../middlewares/matchBodyValidate';
 import Match from '../database/models/match';
 import MatchService from '../services/matchService';
 import MatchController from '../controllers/matchController';
+import LoginBodyValidate from '../middlewares/loginBodyValidate';
 
 export default class UserRouter {
   public router: Router;
@@ -19,9 +21,21 @@ export default class UserRouter {
 
   public addRoute() {
     this.router.get(`/${this.endpoint}`, this.controller.getAll);
-    this.router.get(
-      `/${this.endpoint}/:id`,
-      this.controller.getById,
+    this.router.get(`/${this.endpoint}/:id`, this.controller.getById);
+    this.router.use(`/${this.endpoint}`, LoginBodyValidate.tokenValidate);
+    this.router.post(
+      `/${this.endpoint}`,
+      MatchBodyValidate.validadeTeamIds,
+      MatchBodyValidate.validateTeamsEquals,
+      MatchBodyValidate.validadeGoalsTeam,
+      MatchBodyValidate.validadeInProgress,
+      this.controller.createMatch,
     );
+    this.router.patch(
+      `/${this.endpoint}/:id`,
+      MatchBodyValidate.validadeGoalsTeam,
+      this.controller.updateMatch,
+    );
+    this.router.patch(`/${this.endpoint}/:id/finish`, this.controller.changeProgress);
   }
 }
