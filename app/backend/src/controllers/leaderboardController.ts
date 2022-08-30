db124d91138e1b8564c5f602b34d450d12fcca02
patrik <pcr_batista@hotmail.com>
2022-08-30 16:31:49 -0300

    feat: implemetenta getLeaderboardAway

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -7,10 +7,24 @@ export default class LeaderboardController {
   constructor(leaderboardService: LeaderboardService) {
     this.leaderboardService = leaderboardService;
     this.getLeaderboardHome = this.getLeaderboardHome.bind(this);
+    this.getLeaderboardAway = this.getLeaderboardAway.bind(this);
+    this.getLeaderboard = this.getLeaderboard.bind(this);
   }
 
   public async getLeaderboardHome(_req: Request, res: Response, _next:NextFunction) {
     const result = await this.leaderboardService.getLeaderboardHome();
+    return res.status(200).json(result);
+  }
+
+  public async getLeaderboardAway(_req: Request, res: Response, _next:NextFunction) {
+    const result = await this.leaderboardService.getLeaderboardAway();
+    return res.status(200).json(result);
+  }
+
+  public async getLeaderboard(_req: Request, res: Response, _next:NextFunction) {
+    const resultAway = await this.leaderboardService.getLeaderboardAway();
+    const resultHome = await this.leaderboardService.getLeaderboardHome();
+    const result = LeaderboardService.getLeaderboard(resultAway, resultHome);
     return res.status(200).json(result);
   }
 }
