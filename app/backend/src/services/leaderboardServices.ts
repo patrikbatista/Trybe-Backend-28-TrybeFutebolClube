51012ecfba9d0583a5b5d9396ce09443079ff6c6
patrik <pcr_batista@hotmail.com>
2022-08-30 16:31:08 -0300

    feat: implementa metodo estatico getLeaderBoard

--- app/backend/src/services/leaderboardServices.ts
+++ app/backend/src/services/leaderboardServices.ts
@@ -13,7 +13,7 @@ export default class Leaderboard {
     this.matchModel = matchModel;
   }
 
-  private static matchesForEach(matches: IMatch[], team: ITeam, leaderboard: ILeaderboard) {
+  private static matchesForEachHome(matches: IMatch[], team: ITeam, leaderboard: ILeaderboard) {
     const leaderboardCopy = { ...leaderboard };
     matches.forEach((match) => {
       if (match.homeTeam === team.id && match.inProgress === false) {
@@ -34,7 +34,28 @@ export default class Leaderboard {
     return leaderboardCopy;
   }
 
-  private static matchesMap(team: ITeam, matches: IMatch[]) {
+  private static matchesForEachAway(matches: IMatch[], team: ITeam, leaderboard: ILeaderboard) {
+    const leaderboardCopy = { ...leaderboard };
+    matches.forEach((match) => {
+      if (match.awayTeam === team.id && match.inProgress === false) {
+        if (match.awayTeamGoals > match.homeTeamGoals) {
+          leaderboardCopy.totalPoints += 3;
+          leaderboardCopy.totalVictories += 1;
+        } else if (match.awayTeamGoals === match.homeTeamGoals) {
+          leaderboardCopy.totalPoints += 1;
+          leaderboardCopy.totalDraws += 1;
+        } else leaderboardCopy.totalLosses += 1;
+
+        leaderboardCopy.goalsFavor += match.awayTeamGoals;
+        leaderboardCopy.goalsOwn += match.homeTeamGoals;
+        leaderboardCopy.totalGames += 1;
+      }
+    });
+    leaderboardCopy.goalsBalance = leaderboardCopy.goalsFavor - leaderboardCopy.goalsOwn;
+    return leaderboardCopy;
+  }
+
+  private static matchesMap(team: ITeam, matches: IMatch[], isTeamHome: boolean) {
     const initialLeaderboard: ILeaderboard = {
       name: team.teamName,
       totalPoints: 0,
@@ -47,8 +68,10 @@ export default class Leaderboard {
       goalsBalance: 0,
       efficiency: '0',
     };
+    let leaderboard: ILeaderboard;
+    if (isTeamHome) leaderboard = Leaderboard.matchesForEachHome(matches, team, initialLeaderboard);
+    else leaderboard = Leaderboard.matchesForEachAway(matches, team, initialLeaderboard);
 
-    const leaderboard = Leaderboard.matchesForEach(matches, team, initialLeaderboard);
     leaderboard.efficiency = (
       (leaderboard.totalPoints / (leaderboard.totalGames * 3)) * 100).toFixed(2);
 
@@ -79,9 +102,40 @@ export default class Leaderboard {
     const teams = await this.teamModel.findAll();
     const matches = await this.matchModel.findAll();
 
-    const leaderboard = teams.map((team) => Leaderboard.matchesMap(team, matches));
+    const leaderboard = teams.map((team) => Leaderboard.matchesMap(team, matches, true));
 
     const leaderboardSorted = leaderboard.sort((a, b) => Leaderboard.leaderboardSort(a, b));
     return leaderboardSorted;
+  }
+
+  public async getLeaderboardAway() {
+    const teams = await this.teamModel.findAll();
+    const matches = await this.matchModel.findAll();
+
+    const leaderboard = teams.map((team) => Leaderboard.matchesMap(team, matches, false));
+
+    const leaderboardSorted = leaderboard.sort((a, b) => Leaderboard.leaderboardSort(a, b));
+    return leaderboardSorted;
+  }
+
+  public static getLeaderboard(leaderboardHome: ILeaderboard[], leaderboardAway: ILeaderboard[]) {
+    const leaderboards = leaderboardHome.map((home) => {
+      const homeCopy = { ...home };
+      const away = leaderboardAway.find((awayTeam) => homeCopy.name === awayTeam.name);
+      if (away) {
+        homeCopy.totalVictories += away.totalVictories;
+        homeCopy.totalPoints += away.totalPoints;
+        homeCopy.totalLosses += away.totalLosses;
+        homeCopy.totalGames += away.totalGames;
+        homeCopy.totalDraws += away.totalDraws;
+        homeCopy.goalsOwn += away.goalsOwn;
+        homeCopy.goalsFavor += away.goalsFavor;
+        homeCopy.goalsBalance = homeCopy.goalsFavor - homeCopy.goalsOwn;
+        homeCopy.efficiency = (
+          (homeCopy.totalPoints / (homeCopy.totalGames * 3)) * 100).toFixed(2);
+      }
+      return homeCopy;
+    });
+    return leaderboards.sort((a, b) => Leaderboard.leaderboardSort(a, b));
   }
 }
