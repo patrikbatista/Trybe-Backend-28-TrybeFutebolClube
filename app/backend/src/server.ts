1870f5dffc2139b2aa0180ba3a3ffa355fb637fa
patrik <pcr_batista@hotmail.com>
2022-08-31 10:40:12 -0300

    refactor: adaptacoes necessarias para rodar o teste e o server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,30 +1,6 @@
-import { App } from './app';
+import { application } from './app';
 import 'dotenv/config';
-import UserRouter from './routes/userRoutes';
-import TeamRoutes from './routes/teamRoutes';
-import MatchRoutes from './routes/macthRoutes';
-import LeaderboardRoutes from './routes/leaderboardRoutes';
 
 const PORT = process.env.APP_PORT || 3001;
 
-// new App().start(PORT);
-
-const app = new App();
-
-const userRouter = new UserRouter('login');
-userRouter.addRoute();
-app.addRouter(userRouter.router);
-
-const teamRoutes = new TeamRoutes('teams');
-teamRoutes.addRoute();
-app.addRouter(teamRoutes.router);
-
-const macthRoutes = new MatchRoutes('matches');
-macthRoutes.addRoute();
-app.addRouter(macthRoutes.router);
-
-const leaderboardRoutes = new LeaderboardRoutes('leaderboard');
-leaderboardRoutes.addRoute();
-app.addRouter(leaderboardRoutes.router);
-
-app.start(PORT);
+application.start(PORT);
